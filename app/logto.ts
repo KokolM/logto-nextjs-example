a1e08957976202cbd925e3d23daf0ae9d52fa9e7
Matej Kokol <matej.kokol@centrum.sk>
2024-12-07 15:31:33 +0100

    added encryption key

--- app/logto.ts
+++ app/logto.ts
@@ -4,9 +4,5 @@ export const logtoConfig = {
   appSecret: process.env.LOGTO_APP_SECRET ?? '',
   baseUrl: process.env.LOGTO_BASE_URL ?? '',
   cookieSecret: process.env.LOGTO_COOKIE_SECRET ?? '',
-  cookieSecure: process.env.NODE_ENV === 'production',
-  storage: {
-    type: 'cookie',
-    encryptionKey: process.env.LOGTO_ENCRYPTION_KEY, // Use environment variable
-  },
+  cookieSecure: process.env.NODE_ENV === 'production'
 }
